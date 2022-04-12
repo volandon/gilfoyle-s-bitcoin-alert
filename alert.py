e1b2475a074e046a84747ea0c58644a9a7d69ef4
volandon <lucas.landon.val@gmail.com>
2022-04-11 20:36:27 -0400

    files

---
+++ alert.py
@@ -0,0 +1,45 @@
+import requests
+from time import sleep
+import config_s
+from playsound import playsound as unix_sound
+from winsound import SND_FILENAME,PlaySound
+import platform
+
+
+file = 'files/napalm_death.wav'
+key = config_s.key #using AlphaVantage Bitcoin exchange rate API on sepate module config_s
+url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey={key}'
+
+request = requests.get(url).json()
+
+value = float(request["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+
+while True:
+    request = requests.get(url).json()
+    value_new = float(request["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+    if value == value_new:
+        value_new = float(request["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+        print(f'No changes - previous {value} - new {value_new}')
+        sleep(15)
+        
+    elif value > value_new:
+        if platform.system() == 'Windows':
+            PlaySound(file,SND_FILENAME)
+        else:
+            unix_sound(file)
+       
+        print(f'Music! - previous {value} - new {value_new}')
+        sleep(15)
+
+    else:
+        print(f'still looking - previous {value} - new {value_new}')
+        sleep(15)
+
+    request = requests.get(url).json()
+    value = float(request["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+    sleep(5)
+        
+
+
+
+
